1774faa501ee1084c37fe768999a24b729d46c5c
sbateca <sbtk87@hotmail.com>
2024-11-26 17:36:44 -0500

    [ML] Adjustmens on GW resource

--- infrastructure/common/gateway/main.tf
+++ infrastructure/common/gateway/main.tf
@@ -5,7 +5,7 @@ resource "aws_api_gateway_rest_api"  "magneto_api" {
 resource "aws_api_gateway_resource" "api_backend_gateway" {
   rest_api_id = aws_api_gateway_rest_api.magneto_api.id
   parent_id   = aws_api_gateway_rest_api.magneto_api.root_resource_id
-  path_part   = "backend"
+  path_part   = "/"
 }
 
 resource "aws_api_gateway_method" "proxy_root" {
@@ -15,7 +15,7 @@ resource "aws_api_gateway_method" "proxy_root" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda" {
+resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.magneto_api.id
   resource_id             = aws_api_gateway_resource.api_backend_gateway.id
   http_method             = aws_api_gateway_method.proxy_root.http_method
@@ -40,13 +40,13 @@ resource "aws_api_gateway_integration_response" "proxy" {
 
   depends_on = [
     aws_api_gateway_method.proxy_root,
-    aws_api_gateway_integration.lambda
+    aws_api_gateway_integration.lambda_integration
   ]
 }
 
 resource "aws_api_gateway_deployment" "gw_deployment" {
   depends_on = [
-    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_integration,
   ]
   rest_api_id = aws_api_gateway_rest_api.magneto_api.id
   stage_name  = var.environment_name
